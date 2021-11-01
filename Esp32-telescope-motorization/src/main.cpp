0b92752b225b31779064c182d74fcb9dc9274334
sebastiano <sebastiano.cocchi@gmail.com>
2021-11-01 07:47:34 +0100

    Added svg, wifiConnection.ino

--- Esp32-telescope-motorization/src/main.cpp
+++ Esp32-telescope-motorization/src/main.cpp
@@ -1,33 +1,13 @@
 #include <Arduino.h>
-#include <Stepper.h>
-
-const int stepsPerRevolution = 1000;  // change this to fit the number of steps per revolution
-// for your motor
-
-// initialize the stepper library on pins 8 through 11:
-Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+#define ONBOARD_LED  2
 
 void setup() {
-  // set the speed at 60 rpm:
-  myStepper.setSpeed(20);
-  // initialize the serial port:
-  Serial.begin(9600);
+  pinMode(ONBOARD_LED,OUTPUT);
 }
 
 void loop() {
-  myStepper.step(-stepsPerRevolution);
+  delay(1000);
+  digitalWrite(ONBOARD_LED,HIGH);
+  delay(100);
+  digitalWrite(ONBOARD_LED,LOW);
 }
-
-
-// #define ONBOARD_LED  2
-
-// void setup() {
-//   pinMode(ONBOARD_LED,OUTPUT);
-// }
-
-// void loop() {
-//   delay(1000);
-//   digitalWrite(ONBOARD_LED,HIGH);
-//   delay(100);
-//   digitalWrite(ONBOARD_LED,LOW);
-// }
